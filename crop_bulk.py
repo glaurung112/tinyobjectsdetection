4e897a76a089b85024f02139586c510e6f4cf049
Danny Solorzano Mayorga <dannysolorzano71@gmail.com>
2025-06-26 13:00:37 -0600

    Script para generar recortes aleatorios de las imagenes del dataset con sus respectivos labels para mejorar la deteccion de objetos pequeños

--- crop_bulk.py
+++ crop_bulk.py
@@ -1,9 +1,6 @@
-#!python3
-
 import os
 import cv2
 import numpy as np
-import shutil
 import re
 from tqdm import tqdm
 import time
@@ -14,141 +11,53 @@ image_ext = ['.png', '.jpg', '.bmp']
 ONE_CHANNEL = False
 CROP_SIZE = 299
 
-# Argumentos por consola
 parser = argparse.ArgumentParser()
 parser.add_argument('--source', type=str, required=True, help='Carpeta que contiene imágenes y etiquetas YOLO')
 parser.add_argument('--output', type=str, default='cropped_output', help='Carpeta de salida')
 parser.add_argument('--crops', type=int, default=10, help='Número de crops aleatorios por imagen')
 args = parser.parse_args()
 
-# Variables globales
 NUMBER_OF_CROPS = args.crops
 PATH = args.source
 SAVE_DIR = args.output
 
 def natural_sort(images):
-    """
-    Sort the given iterable in the way that computer expect.
-    :param images: iterable to be sorted.
-    :return: sorted iterable.
-    """
+
     def convert(text): return int(text) if text.isdigit() else text.lower()
     def alphanum_key(key): return [convert(c)
-                                   for c in re.split('([0-9]+)', key)] # one or more consecutive digits
+                                   for c in re.split('([0-9]+)', key)]
     return sorted(images, key=alphanum_key)
 
 
 def get_files(path):
-    """Get the list of files in a directory recursively and sort them in natural order.
-    Args:
-        path (string): The path to the directory.
-    Returns:
-        list: full directory path of the image files in list.
-    """
+
     images = []
     for maindir, subdir, file_name_list in os.walk(path):
         for filename in file_name_list:
             apath = os.path.join(maindir, filename)
-            ext = os.path.splitext(apath)[1]  # splitext is for getting the extension(root and ext) to check the extension whether it is acceptible or not
+            ext = os.path.splitext(apath)[1]
             if ext in image_ext:
                 images.append(apath)
     return natural_sort(images)
 
 
 def read_gt(label):
-    """ Read the ground truth file and return the list of labels.
 
-    Args:
-        label (string): The path to the ground truth file.
-
-    Returns:
-        list: list of labels.
-    """
     with open(label) as f:
         labels = []
         Lines = f.readlines()
         for line in Lines:
             label = int(line.strip().split(' ')[0])
-            x = float(line.strip().split(' ')[1])  # x_width
-            y = float(line.strip().split(' ')[2])  # x_height
+            x = float(line.strip().split(' ')[1])
+            y = float(line.strip().split(' ')[2])
             w_r = float(line.strip().split(' ')[3])
             h_r = float(line.strip().split(' ')[4])
             labels.append([label, x, y, w_r, h_r])
     f.close()
     return labels
 
-# def random_crop(t, image, crop_size):
-#     x= t[1]*image.shape[1]
-#     y= t[2]*image.shape[0]
-#     w= t[3]*image.shape[1]
-#     h= t[4]*image.shape[0]
+def random_crop(t, image, crop_size, num_crops = NUMBER_OF_CROPS):
 
-#     xmin = int(round(x-w/2))
-#     ymin = int(round(y-h/2))
-
-#     #randomly select top cropping points
-#     new_ymin, new_xmin = np.random.randint(low = 20, high = crop_size-max(w/2,h/2)-10, size=2)
-
-#     while(ymin-new_ymin<0):
-#         new_ymin=new_ymin-1
-
-#     while(ymin-new_ymin+crop_size>=image.shape[0]):
-#         new_ymin=new_ymin+1
-
-#     while(xmin-new_xmin<0):
-#         new_xmin=new_xmin-1
-
-#     while(xmin-new_xmin+crop_size>=image.shape[1]):
-#         new_xmin=new_xmin+1
-
-#     crop_img = image[ymin-new_ymin : ymin-new_ymin+crop_size, xmin-new_xmin : xmin-new_xmin+crop_size]
-
-#     new_y = (new_ymin+h/2)/crop_img.shape[0]
-#     new_x = (new_xmin+w/2)/crop_img.shape[1]
-#     new_w = t[3]*image.shape[1]/crop_img.shape[1]
-#     new_h = t[4]*image.shape[0]/crop_img.shape[0]
-#     t_new= [0 , round(new_x,6) , round(new_y,6) , round(new_w,6) , round(new_h,6)]
-
-#     return t_new, crop_img
-
-
-# def random_crop(t, image, crop_size):
-#     x = t[1]*image.shape[1]
-#     y = t[2]*image.shape[0]
-
-#     # randomly select new centers
-#     new_y, new_x = np.random.randint(low=50, high=crop_size-50, size=2)
-
-#     # keep new center of ball and 512 crop in image size
-#     # Note: (y-new_y,x-new_x) new (0,0) origin
-
-#     while(y-new_y < 0):
-#         new_y = new_y-1
-
-#     while(y-new_y+crop_size > image.shape[0]):
-#         new_y = new_y+1
-
-#     while(x-new_x < 0):
-#         new_x = new_x-1
-
-#     while(x-new_x+crop_size > image.shape[1]):
-#         new_x = new_x+1
-
-#     crop_img = image[y-new_y:y-new_y+crop_size, x-new_x:x-new_x+crop_size]
-
-#     new_y = new_y/crop_img.shape[0]
-#     new_x = new_x/crop_img.shape[1]
-#     new_w = int(round(t[3]*image.shape[1]))/crop_img.shape[1]
-#     new_h = int(round(t[4]*image.shape[0]))/crop_img.shape[0]
-#     t_new = [0, new_x, new_y, new_w, new_h]
-
-#     return t_new, crop_img
-
-def random_crop(t, image, crop_size, num_crops = NUMBER_OF_CROPS): #bunlarda tek label var, bize uymuyor.
-    """
-    t: list of labels in the whole image
-    """
-    # determine how many labels are in the image
     num_labels = len(t)
     crop_img_list = []
     t_new_list = []
@@ -158,11 +67,10 @@ def random_crop(t, image, crop_size, num_crops = NUMBER_OF_CROPS): #bunlarda tek
 
 
     for _ in range(num_crops):
-        # randomly select new centers (left top corner of the crop)
+
         crop_x = np.random.randint(low=0, high = max(0,image.shape[1]-crop_size), size=1)[0]
         crop_y = np.random.randint(low=0, high= max(0,image.shape[0]-crop_size), size=1)[0]
 
-        # find the labels in this crop
         labels_in_crop = []
         for label in t:
             x = label[1]*image.shape[1]
@@ -178,14 +86,11 @@ def random_crop(t, image, crop_size, num_crops = NUMBER_OF_CROPS): #bunlarda tek
             if xmin >= crop_x and xmax <= crop_x+crop_size and ymin >= crop_y and ymax <= crop_y+crop_size:
                 labels_in_crop.append(label)
 
-        # if there is no label in the crop, select a new crop
         if len(labels_in_crop) == 0:
             continue
 
-        # crop the image
         crop_img = image[crop_y:crop_y+crop_size, crop_x:crop_x+crop_size, :]
 
-        # update the labels
         t_new = []
         for label in labels_in_crop:
             obj = label[0]
@@ -209,30 +114,20 @@ def random_crop(t, image, crop_size, num_crops = NUMBER_OF_CROPS): #bunlarda tek
 
 
 def random_crop_column(t, image, crop_size):
-    """
-    t: list of labels in the whole image
-    """
+
     x = t[1]*image.shape[1]
     w = t[3]*image.shape[1]
 
     x_min = int(round(x-w/2))
     x_max = int(round(x+w/2))
 
-    # randomly select new centers
     crop_x = np.random.randint(low=max(
         0, x_max+1-crop_size), high=min(x_min-1, image.shape[1]-crop_size), size=1)[0]
-
-
-    # keep new center of ball and 512 crop in image size
-    # Note: (y-new_y,x-new_x) new (0,0) origin
 
     crop_img = image[:, crop_x:crop_x+crop_size]
 
     new_x = (x-crop_x)/crop_img.shape[1]
-    # new_w = w/crop_img.shape[1]
-    # new_h = h/crop_img.shape[0]
 
-    # r = (w+h)/2
     new_w = w/crop_img.shape[1]
 
     t_new = [t[0], new_x, t[2], new_w, t[4]]
@@ -242,56 +137,15 @@ def random_crop_column(t, image, crop_size):
 
 def random_crop_column_n(t, image, crop_size):
 
-    # randomly select new centers
     crop_x = np.random.randint(
         low=0, high=image.shape[1]-crop_size-1, size=1)[0]
-
-    # keep new center of ball and 512 crop in image size
-    # Note: (y-new_y,x-new_x) new (0,0) origin
-
+    
     crop_img = image[:, crop_x:crop_x+crop_size]
 
     t_new = t
 
     return t_new, crop_img
 
-"""
-def random_crop(t, image, crop_size):
-    x = t[1]*image.shape[1]
-    y = t[2]*image.shape[0]
-    w = t[3]*image.shape[1]
-    h = t[4]*image.shape[0]
-
-    x_min = int(round(x-w/2))
-    y_min = int(round(y-h/2))
-    x_max = int(round(x+w/2))
-    y_max = int(round(y+h/2))
-
-    # randomly select new centers, play with this
-    crop_x = np.random.randint(low=max(
-        0, x_max+5-crop_size), high=min(x_min-1, image.shape[1]-crop_size), size=1)[0]
-    crop_y = np.random.randint(low=max(
-        0, y_max+5-crop_size), high=min(y_min-1, image.shape[0]-crop_size), size=1)[0]
-
-    # keep new center of ball and 512 crop in image size
-    # Note: (y-new_y,x-new_x) new (0,0) origin
-
-    crop_img = image[crop_y:crop_y+crop_size, crop_x:crop_x+crop_size]  # keep it same
-
-    # do this label update in for loop because there will be many labels
-    new_y = (y-crop_y)/crop_img.shape[0]
-    new_x = (x-crop_x)/crop_img.shape[1]
-    # new_w = w/crop_img.shape[1]
-    # new_h = h/crop_img.shape[0]
-
-    # r = (w+h)/2, normalize them
-    new_w = w/crop_img.shape[1]
-    new_h = h/crop_img.shape[0]
-
-    t_new = [0, new_x, new_y, new_w, new_h]
-
-    return t_new, crop_img
-"""
 def imread_8bit(img_path):
     img_anydept = cv2.imread(img_path, cv2.IMREAD_ANYDEPTH)
     img8_8bit = (img_anydept/256).astype('uint8')
@@ -312,9 +166,6 @@ def main():
 
     i = 0
 
-    # Crop Process
-    # create output folder for processed images
-
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
 
@@ -323,7 +174,7 @@ def main():
     image_files = get_files(data_path)
     for img_path in tqdm(image_files):
 
-        label_path = img_path[:img_path.rfind('.')] + '.txt'  # rfind: index of the last occurance of the specified value, img_path'ın txt'lisi
+        label_path = img_path[:img_path.rfind('.')] + '.txt'
         if not os.path.exists(label_path):
             print(img_path)
             continue
@@ -332,16 +183,12 @@ def main():
 
         current_image = cv2.imread(img_path)
 
-        labels = read_gt(label_path)  # list of labels(list of lists)
+        labels = read_gt(label_path)
         
-
-        # define crop img
         t_new_list, crop_img_list = random_crop(labels, current_image, crop_size=CROP_SIZE)
 
-
-        # save new ground-truth otherfolder
         i = 0
-        for t_new in t_new_list: # t_new represents multiple labels in a single cropped image
+        for t_new in t_new_list: 
             tag = "_crop_" + str(i)
             new_txt = open(os.path.join(save_dir, label_path.split('/')[-1].split(".")[0] + tag + ".txt"), 'w')
             i += 1
@@ -362,14 +209,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-# img_def = cv2.imread(img_path,0)
-# img_anydept = cv2.imread(img_path, cv2.IMREAD_ANYDEPTH)
-# img_8bit_norm_minmax = imread_normalized(img_path)
-# img8_8bit_max = imread_8bit(img_path)
-
-# import matplotlib.pyplot as plt
-
-# plt.imshow(img_anydept,'gray')
-# plt.imshow(img_def,'gray')
-# plt.imshow(img_8bit_norm_minmax,'gray')
-# plt.imshow(img8_8bit_max,'gray')
