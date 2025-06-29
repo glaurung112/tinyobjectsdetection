e987ef847079312c60ad3c217a533e84ad2197d6
Danny Solorzano Mayorga <dannysolorzano71@gmail.com>
2025-06-29 14:06:56 -0600

    Ajustar las dependencias necesarias para el proyecto y para yolov5

--- install_dependencies.py
+++ install_dependencies.py
@@ -5,28 +5,32 @@ import sys
 def pip_install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-print("Instalando dependencias...")
-print()
+print("Instalando dependencias...\n")
 
-# Instalar dependencias de yolov5 en yolov5/requirements.txt
-print("Instalando desde requirements.txt...")
-pip_install("-r /yolov5-cbam/requirements.txt")
-
-# Lista de paquetes
 packages = [
-    "torch",
-    "opencv-python",
-    "scikit-learn",
-    "tqdm",
-    "matplotlib",
-    "pyyaml",
-    "pandas",
-    "requests",
-    "pillow",
-    "seaborn",
+    "torch",  # recomendado instalar manualmente con instrucciones específicas si es necesario CUDA
+    "torchvision",
+    "numpy>=1.22.2",
+    "opencv-python>=4.1.1",
+    "pillow>=7.1.2",
+    "pyyaml>=5.3.1",
+    "psutil",
+    "matplotlib>=3.3",
+    "requests>=2.23.0",
+    "scipy>=1.4.1",
+    "tqdm>=4.64.0",
+    "ultralytics>=8.0.147",
+    "thop>=0.1.1",
+    "gitpython>=3.1.30",
+    "setuptools>=65.5.1",
+    "pandas>=1.1.4",
+    "seaborn>=0.11.0",
+    "scikit-learn<=1.1.2", 
     "kagglehub",
 ]
 
 for pkg in packages:
     print(f"Instalando {pkg}...")
     pip_install(pkg)
+
+print("\nTodas las dependencias fueron instaladas.")
